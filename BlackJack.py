e8b0db7e6878a401358aff9aa12b1068a3cda250
Martin PC <thebreadandbutterz@gmail.com>
2021-12-31 00:09:22 -0700

    finished stack value calculator for line 47 of logic package

--- BlackJack.py
+++ BlackJack.py
@@ -6,6 +6,7 @@ stack_value = 0
 # Game Setup
 
 game_start = input("Would you like to play?")
+print('\n')
 game_start = game_start.upper()
 if game_start == "N" or game_start == "NO":
     exit()
@@ -15,9 +16,14 @@ else:
 print("Building deck...")
 game_deck = Variables.Deck()
 
+print('\n')
 print("Shuffling deck...")
 game_deck.shuffle()
 
+dealer = Variables.Player('Dealer')
+player_one = Variables.Player('One')
+
+print('\n')
 game_start = input("Are you ready to start?")
 game_start = game_start.upper()
 if game_start == "N" or game_start == "NO":
@@ -25,40 +31,54 @@ if game_start == "N" or game_start == "NO":
 
 # Game Logic
 
-card_stack = []
-hit = ''
-
-print(f'The dealer has dealt {game_deck.all_cards[0]}')
-
-card_stack.append(game_deck.deal_one())
 while game_on:
 
-    hit = ''
+    player_hit = ''
+    dealer_hit = ''
+
+    print('\n')
+    print('The dealer has dealt cards!')
+
+    if player_hit != 'STAND':
+        player_one.all_cards.append(game_deck.deal_one())
+    if dealer_hit != 'STAND':
+        dealer.all_cards.append(game_deck.deal_one())
+    if player_hit = 'STAND' and dealer_hit = 'STAND':
+        Variables.stack_value_calculate(dealer.all_cards, player_one.all_cards)        
+
+    print('\n')
+    print('Dealer')
+    print('---------------------')
+    print(*dealer.all_cards, sep='\n')
+    print('---------------------')
+    print('\n')
+
+    print('\n')
+    print('Player One')
+    print('---------------------')
+    print(*player_one.all_cards, sep='\n')
+    print('---------------------')
+    print('\n')
 
     while hit != "HIT" and hit != "STAND":
+        print('\n')
         hit = input("Hit or Stand?")
         hit = hit.upper()
 
         if hit == "HIT":
+            print('\n')
             print(f'The dealer has dealt {game_deck.all_cards[0]}')
             card_stack.append(game_deck.deal_one())
 
+            print('\n')
             print("Cards currently on the table:")
+            print('\n')
             print(*card_stack, sep='\n')
 
             Variables.stack_value_calculate(card_stack)
 
-            if stack_value == 21:
-                print("Blackjack!")
-                print("Thanks for playing!")
-                exit()
-            elif stack_value < 21:
-                break
-            elif stack_value > 21:
-                print("Bust!")
-                print("Thanks for playing!")
-                exit()
         elif hit == "STAND":
+            print('\n')
             print("filler text")
             exit()
 
--- Variables.py
+++ Variables.py
@@ -4,7 +4,7 @@
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack','Queen','King','Ace')
 
-values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':0}
 
 
 import random
@@ -46,15 +46,58 @@ class Deck():
 
 # card stack value calculating function
 
-def stack_value_calculate(card_stack):
+def stack_value_calculate(dealer, player_one):
 
-    global stack_value
-    stack_value = 0
+    dealer_value = 0
+    player_one_value = 0
 
-    for x in card_stack:
-        stack_value += x.value
+
+    for x in dealer:
+        if x.rank == 'Ace':
+            if dealer_value + 10 < 21:
+                dealer_value += 10
+            else:
+                dealer_value += 1
+        dealer_value += x.value
     
-    print(f'{stack_value}')
+    print('\n')
+    print(f'The dealer is at {dealer_value}')
+
+    for x in player_one:
+        if x.rank == 'Ace':
+            if player_one_value + 10 < 21:
+                player_one_value += 10
+            else:
+                player_one_value += 1
+        player_one_value += x.value
+    
+    print('\n')
+    print(f'Player One is at {player_one_value}')
+
+    if dealer_value == player_one_value:
+        print('Draw!')
+    elif dealer_value > player_one_value:
+        print("The dealer wins this round!")
+        player_one.cash -= 5
+        dealer.cash += 5
+    else:
+        print("Player One wins this round!")
+        dealer.cash -= 5
+        player_one.cash += 5
+        
 
 
 # Player class definition (TBD)
+
+class Player():
+
+    def __init__(self, name):
+        self.name = name
+        self.cash = 25
+        self.all_cards = []
+
+    def remove_all(self):
+        
+        for x in self.all_cards:
+            self.all_cards.pop(0)
+
